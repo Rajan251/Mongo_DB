022afc7a7d71631eb1a2b5a0d2a211107d8bd7bf
Raje <rajemishra77@gmail.com>
2024-01-07 14:36:49 +0530

    Practice commit1

--- main.py
+++ main.py
@@ -24,3 +24,18 @@ all_record = collection.find()
 # Printing all records Present in the collection
 for idx, record in enumerate(all_record):
     print(f"{idx}:{record}")
+
+
+# Second_Lab
+
+import pymongo
+
+dbconn = pymongo.MongoClient("mongodb://localhost:27017/")
+
+dbname = 'ineuron'
+
+db=dbconn[dbname]
+
+
+# to find all the database name.
+dbconn.list_database_names()
